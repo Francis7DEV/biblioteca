74966821d676a992e9ffb127bdc092b25cb5dafe
francis <francielfrancasegundo@gmail.com>
2025-06-18 22:25:27 -0300

    Implementado a saudação inicial.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    greeting();
+}
+
+fn greeting() {
+    println!("__________Bem vindo!__________\n");
+    thread::sleep(time::Duration::from_secs_f32(1.5));
+    println!("Este código tem como intuito estudar o uso de structs em Rust.");
+    thread::sleep(time::Duration::from_secs_f32(1.6));
+    println!("Faremos isso através de um sistema fictício de gerenciamento de bibliotecas.");
+    thread::sleep(time::Duration::from_secs_f32(2.0));
 }
