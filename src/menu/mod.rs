32e9f14d706eb7c5fe8172cc7cd5d81b0f224736
francis <francielfrancasegundo@gmail.com>
2025-06-20 18:50:14 -0300

    Adicionado comentários em menu/mod.

--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,17 +1,23 @@
+// Importações:
 use crate::clear;
 pub mod library_menu;
 use std::io;
 
+// Menu inicial:
 pub(super) fn initial(version: u8) -> u8 {
+    // Limpa o terminal.
     clear();
+    // String para armazenar a option escolhida.
     let mut option: String = String::new();
     println!("__________Bibliotecas__________\n\n");
+    // Se receber 0 como version:
     if version == 0 {
         println!(
             "Deseja cadastrar uma nova Biblioteca?\n\
             1 - SIM.\n\
             * - SAIR.\n"
         );
+    // Se receber 1 como version:
     } else if version == 1 {
         println!(
             "1 - Acessar biblioteca.\n\
